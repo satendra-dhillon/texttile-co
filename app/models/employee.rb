297c67d80bfa860aafd62dbea2cf45797b3e29c3
satendra <satendra@satendra.nppgl.com>
2017-09-19 18:03:27 +0530

    added barcode functionality

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,4 +3,20 @@ class Employee < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  before_save :genrate_bar_code
+
+
+
+
+  private
+
+  def genrate_bar_code
+  	barcodes = Employee.all.map(&:barcode)
+  	number = rand(1000..9999)
+  	while barcodes.include? number
+  		number = rand(1000..9999)
+  	end
+  	self.barcode = number
+  end
 end
